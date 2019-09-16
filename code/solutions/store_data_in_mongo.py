0cdd976a7f3535ae94cc62974e35fa3620ff103d
cosmos-it <cosmostaban@gmail.com>
2019-09-15 17:35:14 -0700

    Added couple assignments

--- code/solutions/store_data_in_mongo.py
+++ code/solutions/store_data_in_mongo.py
@@ -5,6 +5,9 @@
     UPDATE
     DELETE
 
+    First you need to make sure that you install mondo database on your machin (https://docs.mongodb.com/manual/installation/)
+    Install pymongo to connect to mongo database which runs on localhost:27017
+
 """
 
 from pymongo import MongoClient
@@ -13,12 +16,12 @@ from pymongo import MongoClient
 mongo_client = MongoClient("localhost", 27017)
 
 # The database to access
-database = mongo_client["learning"] 
+database = mongo_client["learning"]
+
 #  Your databse colleciton name
 collection = database["data"]
 
 # Create data to post
-
 record = {
     "title": "Go data to MongoDB",
     "desc": "MongoDB is no SQL database",
@@ -29,18 +32,29 @@ record = {
 object_returned = collection.insert_one(record)
 print(object_returned.inserted_id)
 
+# Find one function returns the first one item in the database
+print(collection.find_one())
+
+print(collection.find())
+
 """
    For you exercise, try the following
+
+    0. Try the insert many function
+
     1. Delete by id
 
     2. Updating specific data
 
-    3. List all data
+    3. Find one data
+
+    4. Find by id
+    
+    5. Find all data
+
+    6. Search the collections
+
+    Complete code will be posted.
 
 """
-
-
-
-
-
 
